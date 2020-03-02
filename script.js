5de28e8b155d80907a31cdcf63253861da050646
18121837 <61277231+18121837@users.noreply.github.com>
2020-03-02 16:44:07 +0100

    Site1.0
    Site vanuit desktop gepakt

---
+++ script.js
@@ -0,0 +1,165 @@
+function functionAfstand() {
+    var afstandChart = document.getElementById('afstand').getContext('2d');
+    Chart.defaults.global.defaultFontSize = 16;
+    Chart.defaults.global.defaultFontColor = '#fff';
+    var massPopChart = new Chart(afstandChart, {
+        type: 'horizontalBar'
+        , data: {
+            labels: ['Raket']
+            , datasets: [{
+                label: 'Km'
+                , data: [340]
+                , backgroundColor: ['rgba(112, 174, 240, 0.35)']
+                , borderColor: ['rgba(112, 174, 240, 0.90)']
+                , borderWidth: [1]
+        }]
+        }
+        , options: {
+            title: {
+                display: false
+            }
+            , scales: {
+                xAxes: [{
+                    display: true
+                    , ticks: {
+                        beginAtZero: true
+                        , steps: 100
+                        , stepValue: 5
+                        , max: 400
+                    }
+        }]
+            }
+            , legend: {
+                display: false
+            , }
+            , tooltips: {
+                enabled: true
+            }
+        }
+    });
+}
+
+function functionSpeed() {
+    var speedChart = document.getElementById('snelheid').getContext('2d');
+    Chart.defaults.global.defaultFontSize = 16;
+    Chart.defaults.global.defaultFontColor = '#fff';
+    var massPopChart = new Chart(speedChart, {
+        type: 'line'
+        , data: {
+            labels: [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
+            , datasets: [
+                {
+                    label: "Falcon 9"
+                    , fillColor: '#fff'
+                    , borderColor: 'RGBA(74,185,192,0.50)'
+                    , backgroundColor: 'RGBA(74,185,192,0.20)'
+                    , pointColor: 'rgba(74, 185, 192, 1)'
+                    , pointStrokeColor: '#fff'
+                    , pointHighlightFill: '#fff'
+                    , pointHighlightStroke: 'rgba(74, 185, 192, 1)'
+                    , data: [200, 230, 400, 420, 430, 500, 550, 570, 600, 800]
+            }
+        , ]
+        }
+    });
+}
+
+function functionGravity() {
+    var gravityChart = document.getElementById('zwaartekracht').getContext('2d');
+    Chart.defaults.global.defaultFontSize = 16;
+    Chart.defaults.global.defaultFontColor = '#fff';
+    var massPopChart = new Chart(gravityChart, {
+        type: 'polarArea'
+        , data: {
+            labels: ['Aarde', 'Shuttle', 'Mars']
+            , datasets: [{
+                label: 'Newton'
+                , data: [
+              9.807
+            , 1.201
+            , 3.711
+          ]
+                , backgroundColor: [
+              'rgba(74, 190, 217, 0.61)'
+            , 'rgba(72, 177, 137, 0.61'
+            , 'rgba(215, 55, 157, 0.61)']
+                , borderWidth: 1
+        , }]
+        }
+        , options: {
+            title: {
+                display: true
+                , text: 'Measurement in Newton'
+            }
+            , scales: {
+                yAxes: [{
+                    display: true
+                    , ticks: {
+                        beginAtZero: true
+                    }
+        }]
+            }
+            , legend: {
+                display: false
+                , position: 'right'
+                , labels: {
+                    fontColor: '#000'
+                }
+            }
+            , tooltips: {
+                enabled: true
+            }
+        }
+    });
+}
+
+function functionWater() {
+    var afstandChart = document.getElementById('water').getContext('2d');
+    Chart.defaults.global.defaultFontSize = 16;
+    Chart.defaults.global.defaultFontColor = '#fff';
+    var massPopChart = new Chart(afstandChart, {
+        type: 'bar'
+        , data: {
+            labels: ['Gallons']
+            , datasets: [{
+                label: 'Gallons'
+                , data: [7]
+                , backgroundColor: ['rgba(204, 182, 62, 0.35)']
+                , borderColor: ['rgba(204, 182, 62, 0.90)']
+                , borderWidth: ['1']
+        , }]
+        }
+        , options: {
+            title: {
+                display: false
+            }
+            , scales: {
+                yAxes: [{
+                    barThickness: 6
+                    , maxBarThickness: 8
+                    , display: true
+                    , ticks: {
+                        beginAtZero: true
+                        , steps: 100
+                        , stepValue: 5
+                        , max: 10
+                    }
+        }]
+            }
+            , legend: {
+                display: false
+            , }
+            , tooltips: {
+                enabled: true
+            }
+        }
+    });
+}
+
+function start() {
+    functionAfstand();
+    functionSpeed();
+    functionGravity();
+    functionWater();
+}
+window.onload = start();
